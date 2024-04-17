5e45d12cc901e378e5f3eeb25f8eab84bd90a3e1
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-04-17 14:09:23 +0200

    feat: snack 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Snack 1
+            //L’utente inserisce due numeri in successione.
+            //Il software stampa il maggiore.
+
+            int numero_uno;
+            int numero_due;
+
+            Console.WriteLine("Inserisci il primo numero");
+            numero_uno = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Inserisci il secondo numero");
+            numero_due = Convert.ToInt32(Console.ReadLine());
+
+            if (numero_uno > numero_due)
+            {
+                Console.WriteLine($"Il numero più grande è {numero_uno}");
+            }else if(numero_uno < numero_due)
+            {
+                Console.WriteLine($"Il numero più grande è {numero_due}");
+            }
+            else
+            {
+                Console.WriteLine("Il numero è uguale");
+            }
+          
         }
     }
 }
